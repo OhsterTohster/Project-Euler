7ad72057405cff08e8195ae01033f9b40cfedafa
OhsterTohster <59667279+OhsterTohster@users.noreply.github.com>
2021-06-17 06:51:28 +0800

    Update q7.py

--- Python/q7.py
+++ Python/q7.py
@@ -1,1 +1,36 @@
 # What is the 10 001st prime number?
+#idk whats wrong with the code but something is wrong somewhere because 10001st prime number would mean 10001 elements in the list and thus 
+# primeNumbers[10000] right?
+import math
+def checkPrime(num):
+    isPrime = True
+    if (num % 2 == 0):
+        isPrime = False
+        return isPrime
+    for i in range(3,math.ceil(math.sqrt(num))+1,2):
+        if (num % i == 0):
+            isPrime = False
+            break
+    return isPrime
+
+def solve():
+    primeNumbers = [2,3,5,7,11,13]
+    currentNumber = 13
+    while True:
+        if (len(primeNumbers) > 10001):
+            break 
+        else:
+            checkValue = checkPrime(currentNumber)
+            if (checkValue == True):
+                primeNumbers.append(currentNumber)
+
+        currentNumber+=2
+    
+    print(primeNumbers[10001])
+    print(len(primeNumbers))
+
+solve()
+
+
+
+
