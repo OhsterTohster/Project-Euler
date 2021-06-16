86fbafcf43d79a0050da609d2b9fa3ca5e04f984
OhsterTohster <59667279+OhsterTohster@users.noreply.github.com>
2021-06-17 07:08:13 +0800

    Update q3.py

--- Python/q3.py
+++ Python/q3.py
@@ -5,21 +5,13 @@
 # tbf just throw the number into factordb and that works too lmao
 
 num = 600851475143
-largestNumber = 0
-for i in range(2, num+1):
-    isPrime = False
-    if (num % i == 0):
-        isPrime = True
-        
-        for j in range(2,i//2 +1):
-            if (i % j == 0):
-                isPrime = False
-                break
-    
-    if (isPrime == True):
-        largestNumber = i
-        print(largestNumber)
+divideNumber = 2
+while divideNumber < num:
+    if (num % divideNumber == 0):
+        num /= divideNumber
+        divideNumber -= 1
+    divideNumber+=1
 
 
-print("Largest prime factor is", largestNumber)
+print("Largest prime factor is", num)
     
